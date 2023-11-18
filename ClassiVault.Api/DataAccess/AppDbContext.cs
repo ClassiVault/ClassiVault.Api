28ce0e0ac2ab870669a1537c2c95db188e5b91ea
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2023-11-19 01:59:16 +0300

    refactor: Change namespace declaration

--- ClassiVault.Api/DataAccess/AppDbContext.cs
+++ ClassiVault.Api/DataAccess/AppDbContext.cs
@@ -1,34 +1,33 @@
+namespace ClassiVault.Api.DataAccess;
+
 using ClassiVault.Api.DataAccess.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ClassiVault.Api.DataAccess
+public class AppDbContext : IdentityDbContext<User>
 {
-    public class AppDbContext : IdentityDbContext<User>
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Vault> Vaults { get; set; }
+    public DbSet<EncryptionKeyInfo> EncryptionKeyInfos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        base.OnModelCreating(modelBuilder);
 
-        public DbSet<Vault> Vaults { get; set; }
-        public DbSet<EncryptionKeyInfo> EncryptionKeyInfos { get; set; }
+        // Configure the primary keys
+        modelBuilder.Entity<Vault>().HasKey(pv => pv.Id);
+        modelBuilder.Entity<EncryptionKeyInfo>().HasKey(eki => eki.Id);
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+        // Configure the foreign key relationships
+        modelBuilder.Entity<Vault>()
+            .HasOne<User>(pv => pv.User)
+            .WithMany(u => u.Vaults)
+            .HasForeignKey(pv => pv.UserId);
 
-            // Configure the primary keys
-            modelBuilder.Entity<Vault>().HasKey(pv => pv.Id);
-            modelBuilder.Entity<EncryptionKeyInfo>().HasKey(eki => eki.Id);
-
-            // Configure the foreign key relationships
-            modelBuilder.Entity<Vault>()
-                .HasOne<User>(pv => pv.User)
-                .WithMany(u => u.Vaults)
-                .HasForeignKey(pv => pv.UserId);
-
-            modelBuilder.Entity<EncryptionKeyInfo>()
-                .HasOne<User>(eki => eki.User)
-                .WithMany(u => u.EncryptionKeyInfos)
-                .HasForeignKey(eki => eki.UserId);
-        }
+        modelBuilder.Entity<EncryptionKeyInfo>()
+            .HasOne<User>(eki => eki.User)
+            .WithMany(u => u.EncryptionKeyInfos)
+            .HasForeignKey(eki => eki.UserId);
     }
 }
--- ClassiVault.Api/DataAccess/Models/User.cs
+++ ClassiVault.Api/DataAccess/Models/User.cs
@@ -1,14 +1,13 @@
+namespace ClassiVault.Api.DataAccess.Models;
+
 using Microsoft.AspNetCore.Identity;
 
-namespace ClassiVault.Api.DataAccess.Models
-{
-    public class User : IdentityUser {
-        public ICollection<Vault> Vaults { get; set; }
-        public ICollection<EncryptionKeyInfo> EncryptionKeyInfos { get; set; }
+public class User : IdentityUser {
+    public ICollection<Vault> Vaults { get; set; }
+    public ICollection<EncryptionKeyInfo> EncryptionKeyInfos { get; set; }
 
-        public User() {
-            Vaults = new List<Vault>();
-            EncryptionKeyInfos = new List<EncryptionKeyInfo>();
-        }
+    public User() {
+        Vaults = new List<Vault>();
+        EncryptionKeyInfos = new List<EncryptionKeyInfo>();
     }
 }
